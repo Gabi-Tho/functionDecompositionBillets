e5bcc3953b37475f4abf0ea3326f01a2a9a202db
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-04-26 13:45:41 -0400

    complete table

--- DecompositionBillets.php
+++ DecompositionBillets.php
@@ -1,123 +1,92 @@
 <?php
-
-test commit
-
 function decompositionBillet($money){
     $hundredBills = $money/100;
     settype($hundredBills, "integer");
-    if($hundredBills != 0){
-        echo $hundredBills." "."billets de 100"."<br>";
+    if($hundredBills != 0){ ?>
+        <tr>
+            <td> <?=  $hundredBills;?> </td>
+            <td>billets de 100<br></td>
+        </tr><?php
         $money = $money % 100;
     }
 
-
     $fiftyBills = $money/50;
     settype($fiftyBills, "integer");
-    if($fiftyBills != 0){
+    if($fiftyBills != 0){ ?>
+        <tr>
+            <td class="margin"> <?=  $fiftyBills." "."billets"." "."de";?> </td>
+            <td> <?=  "50"."<br>";?></td>
+        </tr><?php
         settype($fiftyBills, "integer");
-        echo $fiftyBills." "."billets de 50"."<br>";
         $money = $money % 20;
     }
-
 
     $twentyBills = $money/20;
     settype($twentyBills, "integer");
-    if($twentyBills != 0){
+    if($twentyBills != 0){ ?>
+        <tr>
+            <td> <?=  $twentyBills." "."billets"." "."de";?> </td>
+            <td> <?=  "20"."<br>";?></td>
+        </tr><?php
         settype($twentyBills, "integer");
-        echo $twentyBills." "."billets de 20"."<br>";
         $money = $money % 20;
     }
 
-
     $tenBills = $money/10;
     settype($tenBills, "integer");
-    if($tenBills != 0){
-        echo $tenBills." "."billets de 10"."<br>";
+    if($tenBills != 0){ ?>
+        <tr>
+            <td> <?=  $tenBills." "."billets"." "."de";?> </td>
+            <td> <?=  "10"."<br>";?></td>
+        </tr><?php
         $money = $money % 10;
     }
 
-
     $fiveBills = $money/5;
     settype($fiveBills, "integer");
-    if($fiveBills != 0){
-    echo $fiveBills." "."billets de 5"."<br>";
+    if($fiveBills != 0){ ?>
+        <tr>
+            <td> <?=  $fiveBills." "."billets"." "."de";?> </td>
+            <td> <?=  "5"."<br>";?></td>
+        </tr><?php
     $money = $money % 5;
     }
 
-
     $twoBills = $money/2;
     settype($twoBills, "integer");
-    if($twoBills != 0){
+    if($twoBills != 0){ ?>
+        <tr>
+            <td> <?=  $twoBills." "."billets"." "."de";?> </td>
+            <td> <?=  "5"."<br>";?></td>
+        </tr><?php
         $money = $money % 2;
-        echo $twoBills." "."billets de 2"."<br>";
     }
 
     $oneBills = $money/1;
     settype($oneBills, "integer");
-    if($oneBills != 0){
+    if($oneBills != 0){ ?>
+        <tr>
+            <td> <?=  $oneBills." "."billets"." "."de";?> </td>
+            <td> <?=  "5"."<br>";?></td>
+        </tr><?php
         $money = $money % 1;
         echo $oneBills." "."billet de 1"."<br>";
     }
 
 }
+?>
 
+<h2>Decomposition de 3844</h2>
 
+<?php
 echo decompositionBillet(3844);
 
-// $hundredBills = $money/100;
-// settype($hundredBills, "integer");
-// if($hundredBills != 0){
-//     echo $hundredBills." "."billets de 100"."<br>";
-//     $money = $money % 100;
-// }
+?>
 
+<h2> Decomposition de 5266 </h2>
 
-// $fiftyBills = $money/50;
-// settype($fiftyBills, "integer");
-// if($fiftyBills != 0){
-//     settype($fiftyBills, "integer");
-//     echo $fiftyBills." "."billets de 50"."<br>";
-//     $money = $money % 20;
-// }
+<?php
 
-
-// $twentyBills = $money/20;
-// settype($twentyBills, "integer");
-// if($twentyBills != 0){
-//     settype($twentyBills, "integer");
-//     echo $twentyBills." "."billets de 20"."<br>";
-//     $money = $money % 20;
-// }
-
-
-// $tenBills = $money/10;
-// settype($tenBills, "integer");
-// if($tenBills != 0){
-//     echo $tenBills." "."billets de 10"."<br>";
-// $money = $money % 10;
-// }
-
-
-// $fiveBills = $money/5;
-// settype($fiveBills, "integer");
-// if($fiveBills != 0){
-//     echo $fiveBills." "."billets de 5"."<br>";
-// $money = $money % 5;
-// }
-
-
-// $twoBills = $money/2;
-// settype($twoBills, "integer");
-// if($twoBills != 0){
-//     $money = $money % 2;
-//     echo $twoBills." "."billets de 2"."<br>";
-// }
-
-// $oneBills = $money/1;
-// settype($oneBills, "integer");
-// if($oneBills != 0){
-//     $money = $money % 1;
-//     echo $oneBills." "."billet de 1"."<br>";
-// }
+echo decompositionBillet(5266);
 
 ?>
---
+++ extra.php
@@ -0,0 +1,77 @@
+// <html>
+//     <head></head>
+//     <body>
+//         <h2>Decomposition de Billets</h2>
+//         <table>
+//             <tr>
+//                 <td><?= decompositionBillet(3844)?></td>
+<!-- //             </tr>
+//             <tr>
+//                 <td><?= decompositionBillet(5266)?></td>
+//             </tr>
+            
+//         </table>
+//     </body>
+// </html>
+
+// echo decompositionBillet(3844);
+
+// ?> -->
+
+
+// $hundredBills = $money/100;
+// settype($hundredBills, "integer");
+// if($hundredBills != 0){
+//     echo $hundredBills." "."billets de 100"."<br>";
+//     $money = $money % 100;
+// }
+
+
+// $fiftyBills = $money/50;
+// settype($fiftyBills, "integer");
+// if($fiftyBills != 0){
+//     settype($fiftyBills, "integer");
+//     echo $fiftyBills." "."billets de 50"."<br>";
+//     $money = $money % 20;
+// }
+
+
+// $twentyBills = $money/20;
+// settype($twentyBills, "integer");
+// if($twentyBills != 0){
+//     settype($twentyBills, "integer");
+//     echo $twentyBills." "."billets de 20"."<br>";
+//     $money = $money % 20;
+// }
+
+
+// $tenBills = $money/10;
+// settype($tenBills, "integer");
+// if($tenBills != 0){
+//     echo $tenBills." "."billets de 10"."<br>";
+// $money = $money % 10;
+// }
+
+
+// $fiveBills = $money/5;
+// settype($fiveBills, "integer");
+// if($fiveBills != 0){
+//     echo $fiveBills." "."billets de 5"."<br>";
+// $money = $money % 5;
+// }
+
+
+// $twoBills = $money/2;
+// settype($twoBills, "integer");
+// if($twoBills != 0){
+//     $money = $money % 2;
+//     echo $twoBills." "."billets de 2"."<br>";
+// }
+
+// $oneBills = $money/1;
+// settype($oneBills, "integer");
+// if($oneBills != 0){
+//     $money = $money % 1;
+//     echo $oneBills." "."billet de 1"."<br>";
+// } -->
+
